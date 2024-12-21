1baa30d5ecf957403006f9a23ec1b64a3eecec22
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2024-12-21 13:13:43 +0530

    `Refactor Experience component to use react-vertical-timeline-component and framer-motion`

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+    date={experience.date}
+    iconStyle={{ background: experience.iconBg }}
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p className="text-secondary text-[16px] font-semibold style={{margin:0}}">
+        {experience.company_name}
+      </p>
+    </div>
+
+    {/*here an unordered list is used to display the points of the experience i.e experience.point array defined in other file*/}
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
+  </VerticalTimelineElement>
+);
+
+//starting of Experience function
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
+      </motion.div>
 
-export default Experience
+      {/* here a div is made for experience section inside which a VerticalTimeline is used to give a timeline straight vertical line with integrated ui
+          and inside that VerticalTimeline a map function is used to map the experiences array and pass the experience to ExperienceCard component
+          which is used to display the experience in a card format
+          and ExpereinceCard is a component which is defined above now go above for ExperienceCard design
+      */}
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work"); // here work is the idName which is used for scrolling to the section it is route
