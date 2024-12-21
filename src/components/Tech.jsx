640cc21cb05b2d61d6ba33c55b0f48df60daccfc
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2024-12-21 16:44:32 +0530

    `feat: updated Tech and Ball components to render 3D balls with icons from technologies array`

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
+// Tech section starts from this file only
+// in this we map and traverse to each technology element in technologies array
+// and pass the icon of each technology to BallCanvas component
+// then we go to BallCanvas component or Ball file to see the implementation of BallCanvas component
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10 ">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
