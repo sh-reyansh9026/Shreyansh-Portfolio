8f08a3de1df8c0898dbf1bb56a7f643d3e9bded0
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2025-02-14 15:25:20 +0530

    Updated Hero section visibility issue on mobile screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,28 +6,24 @@ import { cn } from "../lib/utils"; // Adjust if needed
 
 export default function HeroSection() {
   return (
-    <LampContainer className="relative flex flex-col items-center justify-center min-h-screen text-center overflow-hidden pt-72">
+    <LampContainer className="relative flex flex-col items-center justify-center min-h-screen text-center overflow-hidden md:pt-72 pt-32 px-4">
       {/* Background Boxes Layer */}
       <div className="inset-0 w-[500px] h-full bg-slate-900 z-10 pointer-events-none" />
       <Boxes />
 
       {/* Main Content */}
-      <div className="relative flex flex-col items-center justify-center pt-72 z-20">
+      <div className="relative flex flex-col items-center justify-center md:pt-72 pt-32 z-20 w-full max-w-screen-md mx-auto">
         <motion.h1
           initial={{ opacity: 0.5, y: 100 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
           className="text-4xl font-medium tracking-tight text-white md:text-7xl -mt-10"
         >
-          <div className="text-center px-4">
+          <div className="text-center">
             <h1 className="text-white">
-              Hi, I'm
-              <span className="text-[#0f0e5a]">Shreyansh</span>
+              Hi, I'm <span className="text-[#0f0e5a]">Shreyansh</span>
             </h1>
-            <p
-              className="mt-4 text-neutral-100 text-center text-lg
-      max-w-3xl leading-[30px]"
-            >
+            <p className="mt-4 text-neutral-100 text-lg leading-[30px]">
               I'm a passionate Software Developer specialized in MERN stack and
               Next.js, building scalable and high-performance web applications.
               With a strong foundation in Data Structures and Algorithms, I
